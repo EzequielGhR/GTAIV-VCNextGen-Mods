a83102f64f2f0a20aeda71b20231667e4532c40d
EzequielGhR <ezequielghra97@gmail.com>
2026-08-02 16:17:36 -0300

    (Tool) Native Scrapper on demand

---
+++ tools/natives_scrapper/models.py
@@ -0,0 +1,62 @@
+from enum import StrEnum
+from pydantic import BaseModel, ConfigDict
+from typing import List, Optional
+
+
+class NativeType(StrEnum):
+    VOID = "VOID"
+    BOOL = "BOOL"
+    INT = "INT"
+    UINT = "UINT"
+    FLOAT = "FLOAT"
+    UNKNOWN = "UNKNOWN"
+
+    @staticmethod
+    def from_string(t: str) -> "NativeType":
+        clean = t.lower().strip()
+        if clean in ["none", "void"]:
+            return NativeType.VOID
+        if clean in ["handle", "int", "integer", "pointer"]:
+            return NativeType.INT
+        if clean in ["bool", "boolean"]:
+            return NativeType.BOOL
+        if clean == "float":
+            return NativeType.FLOAT
+        if clean in ["uint", "unsigned int", "unsigned integer"]:
+            return NativeType.UINT
+
+        return NativeType.UNKNOWN
+    
+
+class NativeParameter(BaseModel):
+    parameter_type: NativeType
+    original_type_string: str
+    description: Optional[str] = None
+    
+    model_config = ConfigDict(from_attributes=True)
+
+    def to_dict(self):
+        return {
+            "type": self.parameter_type.value,
+            "description": self.description,
+            "raw_type": self.original_type_string
+        }
+
+
+class NativeFunction(BaseModel):
+    function_name: str
+    return_type: NativeType
+    return_type_description: str
+    parameters: Optional[List[NativeParameter]] = None
+    description: Optional[str] = None
+
+    model_config = ConfigDict(from_attributes=True)
+
+    def to_dict(self):
+        return {
+            "function": self.function_name,
+            "return_type": self.return_type.value,
+            "return_description": self.return_type_description,
+            "parameters": [p.to_dict() for p in self.parameters] if self.parameters else None,
+            "description": self.description
+        }
---
+++ tools/natives_scrapper/scrapper.py
@@ -0,0 +1,142 @@
+import os
+import json
+import requests
+
+from pathlib import Path
+from typing import Dict, List, Optional
+
+from bs4 import BeautifulSoup
+
+from models import NativeFunction, NativeParameter, NativeType
+
+
+BASE_PART = "https://gtamods.com"
+BASE_URL = f"{BASE_PART}/wiki/List_of_native_functions_(GTA_IV)"
+NATIVE_DATA_PATH = Path(__file__).parent.parent / "native_data"
+
+
+class NativeNotFoundException(Exception): pass
+
+
+def get_soup(url: Optional[str] = None) -> BeautifulSoup:
+    effective_url = url or BASE_URL
+    response = requests.get(effective_url)
+    if not response.ok:
+        raise Exception(f"Failed to fetch Natives from Wiki url: {effective_url}")
+
+    return BeautifulSoup(response.text, "html.parser")
+
+
+def parse_main_list(soup: BeautifulSoup) -> Dict[str, str]:
+    """
+    Returns a dict of native names and their urls to be parsed
+    """
+    def _is_valid(a) -> bool:
+        href = (a or {}).get("href")
+        if not href or not isinstance(href, str):
+            print("Invalid 'a' tag:", a.text)
+            return False
+        return href.startswith("/wiki") and a.text
+        
+    a_tags = soup.select("ul > li > a") or []
+    
+    a_tags = [a for a in a_tags if _is_valid(a)]
+    if not a_tags:
+        raise Exception("Could not find any list of natives");
+
+    return {a.text.strip(): f"{BASE_PART}{a.get("href").strip()}" for a in a_tags}
+
+
+def parse_native_function(soup: BeautifulSoup, name: str) -> NativeFunction:
+    desc_part = soup.find("p")
+    description = desc_part.text if desc_part else None
+    
+    table_rows = soup.select("table tr")
+    param_count_row = table_rows.pop(0)
+    param_count = int(param_count_row.text.split(":")[-1].strip())
+
+    # POP table header
+    table_rows.pop(0)
+    
+    return_type_row = table_rows.pop()
+    rtype, rdesc = return_type_row.findAll("td")
+
+    return_type = NativeType.from_string(rtype.text)
+    return_type_description = rdesc.text.strip()
+
+    params: List[NativeParameter | None] | None = None
+    if not param_count:
+        return NativeFunction(
+            function_name=name,
+            return_type=return_type,
+            return_type_description=return_type_description,
+            parameters=params,
+            description=description
+        )
+
+    params = [None] * param_count
+    
+    for row in table_rows[:param_count]:
+        pnum, ptype, pdesc = row.findAll("td")
+
+        position = int(pnum.text.strip(". ")) - 1
+        parameter_type = NativeType.from_string(ptype.text)
+        parameter_description = pdesc.text.strip()
+
+        params[position] = NativeParameter(
+            parameter_type=parameter_type,
+            original_type_string=ptype.text.strip(),
+            description=parameter_description
+        )
+
+    assert all(params), f"There are null params on list: {params}"
+
+    return NativeFunction(
+        function_name=name,
+        return_type=return_type,
+        return_type_description=return_type_description,
+        parameters=params,
+        description=description
+    )
+
+
+def get_natives_url_ref(force: bool = False) -> dict[str, str]:
+    if not os.path.exists(NATIVE_DATA_PATH / "availability.json") or force:
+        soup = get_soup()
+        data = parse_main_list(soup)
+
+        with open(NATIVE_DATA_PATH / "availability.json", "w") as f:
+            f.write(json.dumps(data, indent=2))
+
+        return data
+
+    with open(NATIVE_DATA_PATH / "availability.json", "r") as f:
+        return json.load(f)
+
+def get_native_list(force: bool = False) -> List[str]:
+    return list(get_natives_url_ref(force).keys())
+            
+
+def get_native_data(function_name: str, force: bool = False) -> dict:
+    clean_name = function_name.strip().upper()
+
+    natives = get_natives_url_ref()
+    if clean_name not in natives:
+        natives = get_natives_url_ref(force=True)
+    if clean_name not in natives:
+        raise NativeNotFoundException(f"The requested native does not exist. Requested: {function_name}. Cleaned: {clean_name}")
+
+    if not os.path.exists(NATIVE_DATA_PATH / f"{clean_name}.json" or force):
+        native_url = natives[clean_name]
+        soup = get_soup(native_url)
+        
+        native_function = parse_native_function(soup, clean_name)
+        native_data = native_function.to_dict()
+
+        with open(NATIVE_DATA_PATH / f"{clean_name}.json", "w") as f:
+            f.write(json.dumps(native_data, indent=2))
+
+        return native_data
+
+    with open(NATIVE_DATA_PATH / f"{clean_name}.json", "r") as f:
+        return json.load(f)
